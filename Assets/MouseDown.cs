7243ba721b41ca1a7f13ee41e2bd1d1932b10f99
ShreeShanthiS <mir.shreeshanthi@gmail.com>
2019-03-12 22:13:40 +0530

    project

---
+++ Assets/MouseDown.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDown : MonoBehaviour
+{
+ 
+
+    private void OnMouseDown()
+    {
+        this.GetComponent<Renderer>().material.color =  Color.red;
+    }
+   
+}
